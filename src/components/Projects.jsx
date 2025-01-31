913f8135eaad7e00ecbd8faedcea06dc526c646c
ahmedmbutt <ahmedmujtababutt02@outlook.com>
2025-02-01 01:12:40 +0500

    fix projects slider

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,84 +1,64 @@
 import { useState } from "react";
 import ProjectDetailsModal from "./ProjectDetailsModal";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { Carousel, Row, Col } from "react-bootstrap";
 import propTypes from "prop-types";
 
 const Projects = ({ resumeProjects }) => {
   const [detailsModalShow, setDetailsModalShow] = useState(false);
   const [selectedProject, setSelectedProject] = useState({});
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1400,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
-      },
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false,
-        },
-      },
-    ],
+  const getProjectsPerSlide = () => {
+    if (window.innerWidth >= 992) return 3;
+    if (window.innerWidth >= 768) return 2;
+    return 1;
   };
 
-  const openDetailsModal = (project) => {
-    setSelectedProject(project);
-    setDetailsModalShow(true);
+  const chunkProjects = (arr) => {
+    const size = getProjectsPerSlide();
+    return Array.from({ length: Math.ceil(arr.length / size) }, (_, i) =>
+      arr.slice(i * size, i * size + size)
+    );
   };
 
-  const closeDetailsModal = () => setDetailsModalShow(false);
+  if (!resumeProjects) return null;
 
-  if (!resumeProjects) {
-    return null;
-  }
+  const projectChunks = chunkProjects(resumeProjects);
 
-  const projectsList = resumeProjects.map((project) => (
-    <div className="col-sm-12 col-md-6 col-lg-4" key={project.title}>
-      <span className="portfolio-item d-block">
-        <div
-          className="foto"
-          style={{ cursor: "pointer" }}
-          onClick={() => openDetailsModal(project)}
-        >
-          <div style={{ maxWidth: "325px" }}>
-            <img
-              src={`/assets/project/${project.images[0]}`}
-              alt={project.title}
-              height="216"
-              style={{
-                marginBottom: 0,
-                paddingBottom: 0,
-                position: "relative",
-              }}
-            />
-            <span className="project-date">{project.startDate}</span>
-            <br />
-            <p className="project-title-settings mt-3">{project.title}</p>
-          </div>
-        </div>
-      </span>
-    </div>
+  const projectItems = projectChunks.map((chunk, index) => (
+    <Carousel.Item key={index}>
+      <Row>
+        {chunk.map((project) => (
+          <Col key={project.title} xs={12} md={6} lg={4}>
+            <span className="portfolio-item d-block">
+              <div
+                className="foto"
+                onClick={() => {
+                  setSelectedProject(project);
+                  setDetailsModalShow(true);
+                }}
+                style={{ cursor: "pointer" }}
+              >
+                <div style={{ maxWidth: "325px" }}>
+                  <img
+                    src={`/assets/project/${project.images[0]}`}
+                    alt={project.title}
+                    height="216"
+                    style={{
+                      marginBottom: 0,
+                      paddingBottom: 0,
+                      position: "relative",
+                    }}
+                  />
+                  <span className="project-date">{project.startDate}</span>
+                  <br />
+                  <p className="project-title-settings mt-3">{project.title}</p>
+                </div>
+              </div>
+            </span>
+          </Col>
+        ))}
+      </Row>
+    </Carousel.Item>
   ));
 
   return (
@@ -88,14 +68,12 @@ const Projects = ({ resumeProjects }) => {
           <i className="fa-solid fa-code-fork">&nbsp;</i>
           <span>{"projects"}</span>
         </h1>
-        <div className="col-md-11 mx-auto">
-          <div className="row mx-auto">
-            <Slider {...settings}>{projectsList}</Slider>
-          </div>
+        <div className="col-md-12 mx-auto">
+          <Carousel>{projectItems}</Carousel>
         </div>
         <ProjectDetailsModal
           show={detailsModalShow}
-          onHide={closeDetailsModal}
+          onHide={() => setDetailsModalShow(false)}
           data={selectedProject}
         />
       </div>
