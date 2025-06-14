0fb101979dfb686262cf97df190681976d694c75
huda zahra <119032530+hzahra55@users.noreply.github.com>
2025-06-14 09:01:12 +0500

    updating projects

--- src/components/ProjectDetailsModal.jsx
+++ src/components/ProjectDetailsModal.jsx
@@ -1,95 +1,195 @@
+// import { Modal } from "react-bootstrap";
+// import AwesomeSlider from "react-awesome-slider";
+// import "../scss/light-slider.module.scss";
+// import "../scss/dark-slider.module.scss";
+// import "react-awesome-slider/dist/custom-animations/scale-out-animation.css";
+// import propTypes from "prop-types";
+
+// const ProjectDetailsModal = ({ data, onHide, show }) => {
+//   if (!data) {
+//     return null;
+//   }
+
+//   const { technologies, images, title, description, url } = data;
+
+//   const techList = technologies?.map((icons, i) => (
+//     <li className="list-inline-item mx-3 px-3" key={i}>
+//       <span>
+//         <div className="text-center">
+//           <i className={icons.class} style={{ fontSize: "300%" }}>
+//             <p className="text-center" style={{ fontSize: "30%" }}>
+//               {icons.name}
+//             </p>
+//           </i>
+//         </div>
+//       </span>
+//     </li>
+//   ));
+
+//   const imgList = images?.map((elem, i) => (
+//     <div key={i} data-src={`/assets/project/${elem}`} />
+//   ));
+//   return (
+//     <Modal
+//       show={show}
+//       onHide={onHide}
+//       size="lg"
+//       aria-labelledby="contained-modal-title-vcenter"
+//       centered
+//       className="modal-inside"
+//     >
+//       <span onClick={onHide} className="modal-close">
+//         <i className="fa-solid fa-xmark fa-3x"></i>
+//       </span>
+//       <div className="col-md-12">
+//         <div className="col-md-10 mx-auto" style={{ paddingBottom: "50px" }}>
+//           <div className="slider-tab">
+//             <span
+//               className="iconify slider-iconfiy"
+//               data-icon="emojione:red-circle"
+//               data-inline="false"
+//               style={{ marginLeft: "5px" }}
+//             ></span>{" "}
+//             &nbsp;{" "}
+//             <span
+//               className="iconify slider-iconfiy"
+//               data-icon="twemoji:yellow-circle"
+//               data-inline="false"
+//             ></span>{" "}
+//             &nbsp;{" "}
+//             <span
+//               className="iconify slider-iconfiy"
+//               data-icon="twemoji:green-circle"
+//               data-inline="false"
+//             ></span>
+//            </div>{/*
+//           <AwesomeSlider
+//             animation="scaleOutAnimation"
+//             className="slider-image light-slider dark-slider"
+//           >
+//             {imgList}
+//           </AwesomeSlider>*/}
+//         </div> 
+//               <div className="text-center mb-4">
+//         <img
+//           src={`/assets/project/${images[0]}`}
+//           alt={title}
+//           className="img-fluid rounded"
+//           style={{ maxHeight: "400px", objectFit: "contain" }}
+//         />
+//       </div>
+
+//         <div className="col-md-10 mx-auto">
+//           <h3 style={{ padding: "5px 5px 0 5px" }}>
+//             {title}
+//             {url ? (
+//               <a
+//                 href={url}
+//                 target="_blank"
+//                 rel="noopener noreferrer"
+//                 className="link-href"
+//               >
+//                 <i
+//                   className="fas fa-external-link-alt"
+//                   style={{ marginLeft: "10px" }}
+//                 ></i>
+//               </a>
+//             ) : null}
+//           </h3>
+//           <p className="modal-description">{description}</p>
+//           <div className="col-md-12 text-center">
+//             <ul className="list-inline mx-auto">{techList}</ul>
+//           </div>
+//         </div>
+//       </div>
+//     </Modal>
+//   );
+// };
+
+// ProjectDetailsModal.propTypes = {
+//   data: propTypes.object,
+//   onHide: propTypes.func,
+//   show: propTypes.bool,
+// };
+
+// export default ProjectDetailsModal;
 import { Modal } from "react-bootstrap";
-import AwesomeSlider from "react-awesome-slider";
 import "../scss/light-slider.module.scss";
 import "../scss/dark-slider.module.scss";
 import "react-awesome-slider/dist/custom-animations/scale-out-animation.css";
 import propTypes from "prop-types";
 
 const ProjectDetailsModal = ({ data, onHide, show }) => {
-  if (!data) {
-    return null;
-  }
+  if (!data) return null;
 
   const { technologies, images, title, description, url } = data;
 
-  const techList = technologies?.map((icons, i) => (
+  const techList = technologies?.map((tech, i) => (
     <li className="list-inline-item mx-3 px-3" key={i}>
-      <span>
-        <div className="text-center">
-          <i className={icons.class} style={{ fontSize: "300%" }}>
-            <p className="text-center" style={{ fontSize: "30%" }}>
-              {icons.name}
-            </p>
-          </i>
-        </div>
-      </span>
+      <div className="text-center">
+        <i className={tech.class} style={{ fontSize: "300%" }}>
+          <p className="text-center" style={{ fontSize: "30%" }}>
+            {tech.name}
+          </p>
+        </i>
+      </div>
     </li>
   ));
 
-  const imgList = images?.map((elem, i) => (
-    <div key={i} data-src={`/assets/project/${elem}`} />
-  ));
   return (
     <Modal
       show={show}
       onHide={onHide}
       size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
       centered
       className="modal-inside"
     >
       <span onClick={onHide} className="modal-close">
         <i className="fa-solid fa-xmark fa-3x"></i>
       </span>
+
       <div className="col-md-12">
         <div className="col-md-10 mx-auto" style={{ paddingBottom: "50px" }}>
           <div className="slider-tab">
-            <span
-              className="iconify slider-iconfiy"
-              data-icon="emojione:red-circle"
-              data-inline="false"
-              style={{ marginLeft: "5px" }}
-            ></span>{" "}
-            &nbsp;{" "}
-            <span
-              className="iconify slider-iconfiy"
-              data-icon="twemoji:yellow-circle"
-              data-inline="false"
-            ></span>{" "}
-            &nbsp;{" "}
-            <span
-              className="iconify slider-iconfiy"
-              data-icon="twemoji:green-circle"
-              data-inline="false"
-            ></span>
+            <span className="iconify slider-iconfiy" data-icon="emojione:red-circle" />
+            &nbsp;
+            <span className="iconify slider-iconfiy" data-icon="twemoji:yellow-circle" />
+            &nbsp;
+            <span className="iconify slider-iconfiy" data-icon="twemoji:green-circle" />
           </div>
-          <AwesomeSlider
-            animation="scaleOutAnimation"
-            className="slider-image light-slider dark-slider"
-          >
-            {imgList}
-          </AwesomeSlider>
-        </div>
-        <div className="col-md-10 mx-auto">
-          <h3 style={{ padding: "5px 5px 0 5px" }}>
-            {title}
-            {url ? (
-              <a
-                href={url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="link-href"
-              >
-                <i
-                  className="fas fa-external-link-alt"
-                  style={{ marginLeft: "10px" }}
-                ></i>
-              </a>
-            ) : null}
-          </h3>
-          <p className="modal-description">{description}</p>
-          <div className="col-md-12 text-center">
-            <ul className="list-inline mx-auto">{techList}</ul>
+
+          <div className="text-center mb-4">
+            <img
+              src={`/assets/project/${images[0]}`}
+              alt={title}
+              className="img-fluid rounded"
+              style={{ maxHeight: "400px", objectFit: "contain" }}
+            />
+          </div>
+
+          <div className="col-md-10 mx-auto">
+            <h3 style={{ padding: "5px 5px 0 5px" }}>
+              {title}
+              {url && (
+                <a
+                  href={url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="link-href"
+                >
+                  <i
+                    className="fab fa-github"
+                    style={{ marginLeft: "10px" }}
+                  ></i>
+                </a>
+              )}
+            </h3>
+
+            <p className="modal-description">{description}</p>
+
+            <div className="col-md-12 text-center">
+              <ul className="list-inline mx-auto">{techList}</ul>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,82 +1,137 @@
+// import { useState } from "react";
+// import ProjectDetailsModal from "./ProjectDetailsModal";
+// import { Carousel, Row, Col } from "react-bootstrap";
+// import propTypes from "prop-types";
+
+// const Projects = ({ resumeProjects }) => {
+//   const [detailsModalShow, setDetailsModalShow] = useState(false);
+//   const [selectedProject, setSelectedProject] = useState({});
+
+//   const getProjectsPerSlide = () => {
+//     if (window.innerWidth >= 992) return 3;
+//     if (window.innerWidth >= 768) return 2;
+//     return 1;
+//   };
+
+//   const chunkProjects = (arr) => {
+//     const size = getProjectsPerSlide();
+//     return Array.from({ length: Math.ceil(arr.length / size) }, (_, i) =>
+//       arr.slice(i * size, i * size + size)
+//     );
+//   };
+
+//   if (!resumeProjects) return null;
+
+//   const projectChunks = chunkProjects(resumeProjects);
+
+//   const projectItems = projectChunks.map((chunk, index) => (
+//     <Carousel.Item key={index}>
+//       <Row>
+//         {chunk.map((project) => (
+//           <Col key={project.title} xs={12} md={6} lg={4}>
+//             <span className="portfolio-item d-block">
+//               <div
+//                 className="foto"
+//                 onClick={() => {
+//                   setSelectedProject(project);
+//                   setDetailsModalShow(true);
+//                 }}
+//                 style={{ cursor: "pointer" }}
+//               >
+//                 <div style={{ maxWidth: "325px" }}>
+//                   <img
+//                     src={`/assets/project/${project.images[0]}`}
+//                     alt={project.title}
+//                     height="216"
+//                     style={{
+//                       marginBottom: 0,
+//                       paddingBottom: 0,
+//                       position: "relative",
+//                     }}
+//                   />
+//                   <span className="project-date">{project.startDate}</span>
+//                   <br />
+//                   <p className="project-title-settings mt-3">{project.title}</p>
+//                 </div>
+//               </div>
+//             </span>
+//           </Col>
+//         ))}
+//       </Row>
+//     </Carousel.Item>
+//   ));
+
+//   return (
+//     <section id="portfolio">
+//       <div className="col-md-12">
+//         <h1 className="section-title" style={{ color: "black" }}>
+//           <i className="fa-solid fa-code-fork">&nbsp;</i>
+//           <span>{"projects"}</span>
+//         </h1>
+//         <div className="col-md-12 mx-auto">
+//           <Carousel>{projectItems}</Carousel>
+//         </div>
+//         <ProjectDetailsModal
+//           show={detailsModalShow}
+//           onHide={() => setDetailsModalShow(false)}
+//           data={selectedProject}
+//         />
+//       </div>
+//     </section>
+//   );
+// };
+
+// Projects.propTypes = {
+//   resumeProjects: propTypes.array,
+// };
+
+// export default Projects;
 import { useState } from "react";
 import ProjectDetailsModal from "./ProjectDetailsModal";
-import { Carousel, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
 import propTypes from "prop-types";
 
 const Projects = ({ resumeProjects }) => {
   const [detailsModalShow, setDetailsModalShow] = useState(false);
-  const [selectedProject, setSelectedProject] = useState({});
-
-  const getProjectsPerSlide = () => {
-    if (window.innerWidth >= 992) return 3;
-    if (window.innerWidth >= 768) return 2;
-    return 1;
-  };
-
-  const chunkProjects = (arr) => {
-    const size = getProjectsPerSlide();
-    return Array.from({ length: Math.ceil(arr.length / size) }, (_, i) =>
-      arr.slice(i * size, i * size + size)
-    );
-  };
+  const [selectedProject, setSelectedProject] = useState(null);
 
   if (!resumeProjects) return null;
 
-  const projectChunks = chunkProjects(resumeProjects);
-
-  const projectItems = projectChunks.map((chunk, index) => (
-    <Carousel.Item key={index}>
-      <Row>
-        {chunk.map((project) => (
-          <Col key={project.title} xs={12} md={6} lg={4}>
-            <span className="portfolio-item d-block">
-              <div
-                className="foto"
-                onClick={() => {
-                  setSelectedProject(project);
-                  setDetailsModalShow(true);
-                }}
-                style={{ cursor: "pointer" }}
-              >
-                <div style={{ maxWidth: "325px" }}>
-                  <img
-                    src={`/assets/project/${project.images[0]}`}
-                    alt={project.title}
-                    height="216"
-                    style={{
-                      marginBottom: 0,
-                      paddingBottom: 0,
-                      position: "relative",
-                    }}
-                  />
-                  <span className="project-date">{project.startDate}</span>
-                  <br />
-                  <p className="project-title-settings mt-3">{project.title}</p>
-                </div>
-              </div>
-            </span>
-          </Col>
-        ))}
-      </Row>
-    </Carousel.Item>
-  ));
-
   return (
-    <section id="portfolio">
-      <div className="col-md-12">
-        <h1 className="section-title" style={{ color: "black" }}>
-          <i className="fa-solid fa-code-fork">&nbsp;</i>
-          <span>{"projects"}</span>
+    <section id="portfolio" className="projects-section py-5">
+      <Container>
+        <h1 className="section-title text-center mb-4">
+          <i className="fa-solid fa-code-fork me-2"></i>Projects
         </h1>
-        <div className="col-md-12 mx-auto">
-          <Carousel>{projectItems}</Carousel>
-        </div>
-        <ProjectDetailsModal
-          show={detailsModalShow}
-          onHide={() => setDetailsModalShow(false)}
-          data={selectedProject}
-        />
-      </div>
+        <Row className="g-4">
+          {resumeProjects.map((project) => (
+            <Col key={project.title} xs={12} sm={6} lg={4}>
+              <Card className="project-card h-100" onClick={() => {
+                setSelectedProject(project);
+                setDetailsModalShow(true);
+              }}>
+                <Card.Img
+                  variant="top"
+                  src={`/assets/project/${project.images[0]}`}
+                  alt={project.title}
+                  className="project-image"
+                />
+                <Card.Body>
+                  <Card.Title className="text-center">
+                    {project.title}
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+
+      <ProjectDetailsModal
+        show={detailsModalShow}
+        onHide={() => setDetailsModalShow(false)}
+        data={selectedProject}
+      />
     </section>
   );
 };
